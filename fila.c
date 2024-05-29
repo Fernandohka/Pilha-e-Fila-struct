e5ddc9f26323726f12c02b2108f0e6b0155b4d08
Fernandohka <167026124+Fernandohka@users.noreply.github.com>
2024-05-29 16:35:37 -0300

    Create fila.c

---
+++ fila.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Fila{
+    int valor;
+    struct Fila *next;
+    struct Fila *prev;
+}Fila;
+
+void enqueue(){
+
+}
+
+void dequeue(){
+
+}
+
+int main(){
+
+}
